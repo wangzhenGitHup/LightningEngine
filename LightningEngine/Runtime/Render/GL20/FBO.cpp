f5a71bb036e7206676a5f9d37b25299c0fb8b6a7
wangzhen <463208406@qq.com>
2020-06-26 23:54:47 +0800

    [LOG]部分帧缓冲接口实现

--- LightningEngine/Runtime/Render/GL20/FBO.cpp
+++ LightningEngine/Runtime/Render/GL20/FBO.cpp
@@ -1,7 +1,186 @@
 #include "FBO.h"
 #include "Render/RenderState.h"
+#include "Debugger/Debugger.h"
 
 namespace LightningEngine
 {
+	FBO::FBO(GLint fbo) : frameBufferObject_(fbo), 
+		width_(512), 
+		height_(512), 
+		rbo_(nullptr)
+	{
+		prevFrameBufferObject_ = 0;
+		clearColor_.Set(0.1f, 0.4f, 0.6f, 1.0f);
+		bSystemFBO_ = !(-1 == fbo);
+	}
+
+	FBO::~FBO()
+	{
+		if (nullptr != rbo_)
+		{
+			delete rbo_;
+			rbo_ = nullptr;
+		}
+
+		if (0 != frameBufferObject_ && !bSystemFBO_)
+		{
+			OGL_CALL(glDeleteFramebuffers(1, &frameBufferObject_));
+		}
+	}
+
+	void FBO::SetClearColor(float r, float g, float b, float a, int idx)
+	{
+	}
+
+	void FBO::Init(int width, int height)
+	{
+		width_ = width;
+		height_ = height;
+		colorBuffer_ = new Texture2D;
+		colorBuffer_->InitDepthTexture(width, height);
+		rbo_ = new RBO;
+		rbo_->Init(width, height);
+		OGL_CALL(glGenFramebuffers(1, &frameBufferObject_));
+		OGL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject_));
+		OGL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer_->textureID_, 0));
+		
+#ifdef LIGHTNING_ENGINE_PLATFORM_ANDROID
+		OGL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rbo_->renderBufferObject_));
+		OGL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo_->renderBufferObject_));
+#elif LIGHTNING_ENGINE_PLATFORM_IOS
+#else
+		OGL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo_->renderBufferObject_));
+#endif
+		int code = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+		if (GL_FRAMEBUFFER_COMPLETE != code)
+		{
+			ErrorPrint("Create fbo fail");
+		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void FBO::Resize(int width, int height)
+	{
+		if (bSystemFBO_)
+		{
+			width_ = width;
+			height_ = height;
+			return;
+		}
+
+		if (0 == frameBufferObject_)
+		{
+			return;
+		}
+
+		if (nullptr != rbo_)
+		{
+			DetachDepthStencilRenderBuffer();
+			delete rbo_;
+			rbo_ = nullptr;
+		}
+
+		if (nullptr != colorBuffer_.ptr_)
+		{
+			DetachColorBuffer(GL_COLOR_ATTACHMENT0);
+			colorBuffer_.Release();
+		}
+
+		if (nullptr != depthComponentBuffer_.ptr_)
+		{
+			depthComponentBuffer_.Release();
+			OGL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthComponentBuffer_->textureID_, 0));
+			glBindFramebuffer(GL_FRAMEBUFFER, prevFrameBufferObject_);
+
+#ifdef LIGHTNING_ENGINE_PLATFORM_ANDROID
+			InitDepthFBOES(width, height);
+#elif LIGHTNING_ENGINE_PLATFORM_IOS
+#else
+			InitDepthFBO(width, height);
+#endif
+		}
+		else
+		{
+			glBindFramebuffer(GL_FRAMEBUFFER, prevFrameBufferObject_);
+			Init(width, height);
+		}
+	}
+
+	void FBO::InitDepthFBO(int width, int height)
+	{
+		width_ = width;
+		height_ = height;
+		colorBuffer_ = new Texture2D;
+		colorBuffer_->InitTexture2D(width, height);
+		depthComponentBuffer_ = new Texture2D;
+		OGL_CALL(glGenFramebuffers(1, &frameBufferObject_));
+		depthComponentBuffer_->InitDepthTexture(width, height);
+		OGL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject_));
+		OGL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer_->textureID_, 0));
+		OGL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthComponentBuffer_->textureID_, 0));
+		
+		int code = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+		if (code != GL_FRAMEBUFFER_COMPLETE)
+		{
+			InfoPrint("create fbo fail");
+		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void FBO::InitDepthFBOES(int width, int height)
+	{
+	}
+
+	void FBO::InitDepthPartWithDefaultSettings(int width, int height)
+	{
+	}
+
+	void FBO::AttachColorBuffer(int attachPoint, Texture2D * pTex)
+	{
+		width_ = pTex->width_;
+		height_ = pTex->height_;
+		colorBuffer_ = pTex;
+		OGL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject_));
+		OGL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + attachPoint, GL_TEXTURE_2D, pTex->textureID_, 0));
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+
+	void FBO::DetachColorBuffer(GLenum attachPoint)
+	{
+		OGL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, attachPoint, GL_TEXTURE_2D, 0, 0));
+	}
+
+	void FBO::DetachRenderBuffer(GLenum attachPoint)
+	{
+		OGL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, attachPoint, GL_RENDERBUFFER, 0));
+	}
+
+	void FBO::DetachDepthStencilRenderBuffer()
+	{
+#ifdef LIGHTNING_ENGINE_PLATFORM_ANDROID
+		DetachRenderBuffer(GL_DEPTH_ATTACHMENT);
+		DetachRenderBuffer(GL_STENCIL_ATTACHMENT);
+#elif LIGHTNING_ENGINE_PLATFORM_IOS
+#else
+		DetachRenderBuffer(GL_DEPTH_STENCIL_ATTACHMENT);
+#endif
+	}
+
+	void FBO::Active()
+	{
+	}
+
+	void FBO::ActiveDepth()
+	{
+	}
+
+	void FBO::Switch()
+	{
+	}
+
+	void FBO::Restore()
+	{
+	}
+
 
 }
