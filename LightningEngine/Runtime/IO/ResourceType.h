523c2b1cb8c5afc8af4988f941959a6434059ba1
wangzhen <463208406@qq.com>
2020-07-15 23:20:36 +0800

    [LOG]资源枚举类型

---
+++ LightningEngine/Runtime/IO/ResourceType.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace LightningEngine
+{
+	enum ResourceType
+	{
+		ResourceTypeNone = 1,
+		ResourceTypePNGImage,
+		ResourceTypeJPGImage,
+		ResourceTypeMP3,
+		ResourceTypeAnimator,
+		ResourceTypeAnimation,
+		ResourceTypeShader,
+		ResourceTypeMaterial,
+		ResourceTypeAtlas,
+		ResourceTypeBMPImage,
+		ResourceTypeScene,
+		ResourceTypeETC1RGB,
+		ResourceTypePVRTCRGBA,
+		ResourceTypePrefab,
+		ResourceTypeFont,
+		ResourceTypeScript,
+		ResourceTypeRawImageLUMINANCE,
+		ResourceTypeBytesData,
+		ResourceTypeTGAImage,
+		ResourceTypeMesh,
+		ResourceTypeModel,
+		ResourceTypeImage,
+		ResourceTypeAvatar,
+		ResourceTypeCount
+	};
+}
