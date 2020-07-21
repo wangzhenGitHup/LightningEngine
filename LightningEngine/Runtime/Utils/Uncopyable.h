93e79f5299043edf1cf8cefcf38c383809006ae2
wangzhen <463208406@qq.com>
2020-07-21 16:40:12 +0800

    [LOG]组织copying动作而设计的类

---
+++ LightningEngine/Runtime/Utils/Uncopyable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace LightningEngine
+{
+	class Uncopyable
+	{
+	protected:
+		Uncopyable() {}
+		~Uncopyable() {}
+
+	private:
+		Uncopyable(const Uncopyable&);
+		Uncopyable& operator=(const Uncopyable&);
+	};
+}
